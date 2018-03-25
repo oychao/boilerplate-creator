ac3d7b108007de0451e9446bed25b6f349dab6c4
Ouyang Chao <ch.k.ouyang@gmail.com>
2018-03-25 15:32:28 +0800

    able to create basic react app now

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,25 @@
 import shebang from 'rollup-plugin-shebang';
+import json from 'rollup-plugin-json';
 
 export default [{
     input: './index.js',
     output: [{
         file: 'bin/bundle.js',
-        format: 'cjs'
+        format: 'cjs',
     }],
     plugins: [
         shebang({
-            shebang: '#!/usr/bin/env node'
+            shebang: '#!/usr/bin/env node',
+        }),
+        json({
+            preferConst: true,
+            indent: '  ',
         })
-    ]
+    ],
+    external: [
+        'fs',
+        'path',
+        'commander',
+        'mkdirp',
+    ],
 }];
---
+++ templates/1/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './style.css';
+
+ReactDOM.render(
+    <h1>Boilerplate Creator</h1>,
+    document.getElementById('root'),
+);
