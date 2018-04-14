05d8e4537f20f67bfa4ab84b65aeb2b81f4deee5
ouyang <chao@ouyang.io>
2018-04-14 09:02:52 +0800

    v1.2.0

--- index.js
+++ index.js
@@ -12,34 +12,53 @@ import { version as VERSION, } from './package.json';
 const { spinnerEcho, } = utils;
 const TEMPLATE_DIR = path.join(__dirname, '..', 'templates');
 const BPC_NAME = chalk.bold.blue(`boilerplate-creator ${VERSION}`);
-const legalTemps = ['npm', 'cli', 'react', 'react-ts',];
+const npmTemps = ['npm', 'npm-ts', 'cli', 'cli-ts',];
+const legalTemps = npmTemps.concat(['react', 'react-ts',]);
+const legalTempsHintStr = legalTemps.reduce((acc, temp) => `${acc}|${chalk.yellow(temp)}`, '').slice(1);
 const legalLics = ['MIT', 'WTFPL', 'Apache', 'GPL', 'BSD', 'Mozilla',];
 
 // error if no npm
 if(!shell.which('npm') || !shell.which('node')) {
-    shell.echo(`${chalk.red('Error:')} boilerplate-creator requires npm and nodejs`);
+    shell.echo(`${chalk.red('Error:')} boilerplate-creator requires npm and nodejs.`);
     shell.exit(1);
 }
 
+// build a cli program
 program.name(chalk.green('bpc'))
     .usage(chalk.green('[project name] [options]'))
-    .description(`${BPC_NAME}: create a boilerplate`)
-    .option('-t, --template [temp]', `which template to use [temp] support(${legalTemps.join('|')})`, 'react')
+    .description(`${BPC_NAME}: create a boilerplate quickly`)
+    .option('-t, --template [temp]', `which template to use [temp] support(${legalTempsHintStr})`, 'react')
     .option('-l, --licence [lic]', `generate licence file [lic] e.g.(${legalLics.join('|')})`, 'MIT')
     .option('-f, --force', 'force on non-empty directory')
+    .option('    --ts', 'use typescript, \'bpc demo -t react-ts\' is equivalent to \'bpc demo -t react --ts\'')
     .version(VERSION, '-v, --version')
     .parse(process.argv);
+
+// handle typescript option
+if(program.ts && program.template.slice(-3, 0) !== '-ts') {
+    program.template += '-ts';
+}
 
 // error if ileegal template name
 if(legalTemps.indexOf(program.template) === -1) {
     shell.echo(`${chalk.red('Error:')} illegal template '${chalk.red(program.template)}'`);
+    shell.echo(`Support (${legalTempsHintStr}).`);
     shell.exit(1);
 }
 
 // get destination name
-const projectName = program.args.shift().replace(/\//g, '');
+let projectName;
+try {
+    projectName = program.args.shift().replace(/\//g, '');
+} catch (e) {
+    // output help document if no project name is given
+    shell.echo();
+    shell.echo(chalk.red('  Illegal Usage!'));
+    program.outputHelp();
+    shell.exit(1);
+}
 // if project is an app, otherwise it's a npm library or a cli tool
-const isApp = program.template !== 'npm' && program.template !== 'cli';
+const isApp = npmTemps.indexOf(program.template) === -1;
 // other variables and constants
 let spinner;
 const start = new Date().getTime();
@@ -79,6 +98,7 @@ jsonfile.read(path.join(TEMPLATE_DIR, program.template, 'package.json')).then(pk
             spinnerEcho(chalk.magentaBright('Get started with following commands: '), 'info');
             shell.echo();
             shell.echo(`   ${chalk.yellow('$')} ${chalk.blueBright(`cd ${projectName}`)}`);
+            shell.echo(`   ${chalk.yellow('$')} # do something`);
             shell.echo(`   ${chalk.yellow('$')} ${chalk.blueBright('npm start')}`);
         } else {
             spinnerEcho(chalk.magentaBright('Edit your code and build the project: '), 'info');
---
+++ templates/cli-ts/src/main.ts
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+export default function(world: string): void {
+    console.log(chalk.yellow(`hello ${world} in typescript`));
+}
