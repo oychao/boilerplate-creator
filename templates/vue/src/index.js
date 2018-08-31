a7597a031ff9e8c44c892d2701dc132e8d5c9183
ouyang-chao <chao@ouyang.io>
2018-08-31 20:00:55 +0800

    [feature] update vue template

--- templates/vue/src/index.js
+++ templates/vue/src/index.js
@@ -1,10 +1,16 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import { sync } from 'vuex-router-sync';
 
 import store from './store';
 import router from './router';
 import app from './components/app.vue';
+
+Vue.use(VueAxios, axios);
+
+console.log(Vue.axios === axios);
 
 const unsync = sync(store, router);
 
